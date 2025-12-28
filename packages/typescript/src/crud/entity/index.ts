1a03922a5b9b3ce5307d29a725b83eef6dd4966e
ds1sqe <ds1sqe@mensakorea.org>
2025-12-28 13:32:13 +0900

    Restructure: Move Python to root, split TypeScript to separate repo

--- packages/typescript/src/crud/entity/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Entity CRUD module exports.
- */
-
-export { EntityManager, type Connection } from './manager.js';
-export { EntityQuery, type SortDirection, type SortSpec } from './query.js';
--- packages/typescript/src/crud/relation/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Relation CRUD module exports.
- */
-
-export { RelationManager, type Connection } from './manager.js';
-export { RelationQuery, type SortDirection, type SortSpec } from './query.js';
